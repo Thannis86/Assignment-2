cc4377bd7f39663b0d27f63b3552f0b8f0db2740
Cameron Burchnall <cameron.burchnall@ntlworld.com>
2024-12-13 13:31:24 +0000

    Added div and placed background image inside it Added image array to JS Added images to files Added buttons Added div for buttons and added that as a const in js Added background div as a const in js

---
+++ scripts.js
@@ -0,0 +1,25 @@
+const imgs = [
+  {
+    src: "./cookie",
+    alt: "A stack of 4 chocolate chip cookies, with a broken up chocolate cookie next to them, all on top of a white cloth on a wooden table.",
+  },
+  {
+    src: "./brownie",
+    alt: "Chocolate brownies, drizzled with chocolate sauce and a mint leaf on parchment paper, on a wooden table",
+  },
+  {
+    src: "./caramelmouse",
+    alt: "A yellow mousse with orange sauce, on a grey plate with jars of fruit and candles in the background",
+  },
+  {
+    src: "./chocolatecake",
+    alt: "A chocolate and coffee cake with coffe beans on a white plate on top of a white wood table",
+  },
+  {
+    src: "./sugarcookie",
+    alt: "A stack of sugar cookies in the shape of stars wrapped in a bow. A whole star anise on top, with a branch from a pine tree and sliced of orange with cinnamon sticks on top in the background",
+  },
+];
+
+const imgslct = document.querySelector(".imgselect");
+const mainimg = document.querySelector(".background");
