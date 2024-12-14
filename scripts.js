00e198650a9912ba18b2ef601526cbda6e5661d8
Cameron Burchnall <cameron.burchnall@ntlworld.com>
2024-12-14 21:12:02 +0000

    Imported buttons from HTML to JS Added images to buttons and resized them Adjusted grid, removing top row Created events for buttons to switch background on click, hide clicked button and unhide any other hidden buttons

--- scripts.js
+++ scripts.js
@@ -21,10 +21,14 @@ const imgs = [
   },
 ];
 
-const img = "./brownie.jpg";
+const btn1 = document.querySelector("#img1");
+const btn2 = document.querySelector("#img2");
+const btn3 = document.querySelector("#img3");
+const btn4 = document.querySelector("#img4");
+const btn5 = document.querySelector("#img5");
 
 const imgslct = document.querySelector(".imgselect");
-const mainimg = document.querySelector(".background");
+const mainimg = document.querySelector("#background");
 
 // https://www.w3schools.com/js/tryit.asp?filename=tryjs_intro_lightbulb
 // Image src selector help
@@ -34,38 +38,99 @@ document.addEventListener("DOMContentLoaded", function () {
     mainimg.src = imgs[0].src;
     mainimg.alt = imgs[0].alt;
     console.log(imgs[0]);
+    btn1.style.display = "none";
   }
 });
 
-const imgcont = document.querySelector("#container");
+btn1.addEventListener("click", () => {
+  btn1.style.display = "none";
+  btn2.style.display = "flex";
+  btn3.style.display = "flex";
+  btn4.style.display = "flex";
+  btn5.style.display = "flex";
+  mainimg.src = imgs[0].src;
+  mainimg.alt = imgs[0].alt;
+});
 
-function createThumbnails() {
-  for (let i = 0; i <= imgs.length; i++) {
-    console.log(imgs[i]);
-    const thumbImg = document.createElement("img");
-    thumbImg.width = 80;
-    thumbImg.height = 45;
-    thumbImg.src = imgs[i].src;
-    imgcont.appendChild(thumbImg);
-  }
-}
+btn2.addEventListener("click", () => {
+  btn1.style.display = "flex";
+  btn2.style.display = "none";
+  btn3.style.display = "flex";
+  btn4.style.display = "flex";
+  btn5.style.display = "flex";
+  mainimg.src = imgs[1].src;
+  mainimg.alt = imgs[1].alt;
+});
 
-createThumbnails(imgs);
+btn3.addEventListener("click", () => {
+  btn1.style.display = "flex";
+  btn2.style.display = "flex";
+  btn3.style.display = "none";
+  btn4.style.display = "flex";
+  btn5.style.display = "flex";
+  mainimg.src = imgs[2].src;
+  mainimg.alt = imgs[2].alt;
+});
 
-// const galleryContainer = document.getElementById("container");
+btn4.addEventListener("click", () => {
+  btn1.style.display = "flex";
+  btn2.style.display = "flex";
+  btn3.style.display = "flex";
+  btn4.style.display = "none";
+  btn5.style.display = "flex";
+  mainimg.src = imgs[3].src;
+  mainimg.alt = imgs[3].alt;
+});
 
-// // Loop through the array and create gallery items
-// for (let i = 0; i < imgs.length; i++) {
-//   const galleryItem = document.createElement("div");
-//   galleryItem.classList.add("gallery-item");
+btn5.addEventListener("click", () => {
+  btn1.style.display = "flex";
+  btn2.style.display = "flex";
+  btn3.style.display = "flex";
+  btn4.style.display = "flex";
+  btn5.style.display = "none";
+  mainimg.src = imgs[4].src;
+  mainimg.alt = imgs[4].alt;
+});
 
-//   const imgElement = document.createElement("img");
-//   imgElement.src = imgs[i].src;
-//   imgElement.alt = imgs[i].src;
+const imgcont = document.querySelector(".container");
 
-//   // Append the image to the gallery item
-//   galleryItem.appendChild(imgElement);
+// function createThumbnails() {
+//   for (let i = 0; i <= imgs.length; i++) {
+//     console.log(imgs[i]);
+//     const thumbImg = document.createElement("img");
+//     thumbImg.width = 80;
+//     thumbImg.height = 45;
+//     thumbImg.src = imgs[i].src;
+//     thumbImg.alt = imgs[i].alt;
+//     imgslct.appendChild(thumbImg);
+//   }
+// }
 
-//   // Append the gallery item to the container
-//   galleryContainer.appendChild(galleryItem);
+//     // if (!Array[i]) {
+//     //   continue;
+// createThumbnails(imgs);
+
+// function createLargeImagesHandler() {
+//   mainimg.innerHTML = null;
+//   const bigImg = document.createElement("img");
+//   bigImg.height = 500;
+//   bigImg.width = 500;
+//   bigImg.src = imgs[2].src;
+//   bigImg.alt = imgs[2].alt;
+//   mainimg.append(bigImg);
+// }
+
+// const testbtn = document.querySelector("#test");
+
+// testbtn.addEventListener("click", () => {
+//   console.log("This worked");
+// });
+
+// // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+// // Splice support
+
+// function remove() {
+//   // imgs.splice(2, 1);
+//   // imgcont.textContent = "It worked";
+//   console.log("The button worked");
 // }
